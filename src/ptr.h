91fa9acfd2d271bb4a9763c68b329296597bc005
Jesse A. Tov <jesse.tov@gmail.com>
2016-05-16 16:29:01 -0500

    Allocator for each type now keeps a pointer to the allocated object.

--- src/ptr.h
+++ src/ptr.h
@@ -4,23 +4,20 @@
 #include "stl.h"
 
 #include <utility>
+#include <unordered_set>
 
 namespace gc
 {
 
+template <typename T>
+class Allocator;
+
 template <typename T, typename = Traceable<T>>
 class traced_ptr
 {
-public:
-    template <typename... Args>
-    static traced_ptr<T> make_traced(Args&&... args)
-    {
-        traced_ptr<T> result;
-        result.pimpl_ = new typename traced_ptr<T>::impl(std::forward<Args>(args)...);
-        return result;
-    }
-
 private:
+    friend class Allocator<T>;
+
     struct impl
     {
         template <typename... Args>
@@ -48,7 +45,35 @@ DEFINE_TRACEABLE(traced_ptr<T>)
 template <typename T, typename... Args>
 traced_ptr<T> make_traced(Args&&... args)
 {
-    return traced_ptr<T>::template make_traced<Args...>(std::forward<Args>(args)...);
+    return Allocator<T>::instance()
+            .template make_traced<Args...>(std::forward<Args>(args)...);
 }
+
+template <typename T>
+class Allocator
+{
+public:
+    static Allocator& instance() noexcept
+    {
+        static Allocator instance_;
+        return instance_;
+    }
+
+    template <typename... Args>
+    traced_ptr<T> make_traced(Args&&... args)
+    {
+        traced_ptr<T> result;
+        result.pimpl_ = new typename traced_ptr<T>::impl(std::forward<Args>(args)...);
+        objects_.insert(result.pimpl_);
+        return result;
+    }
+
+private:
+    Allocator()
+    { }
+
+    std::unordered_set<typename traced_ptr<T>::impl*> objects_;
+    std::unordered_set<typename traced_ptr<T>::impl*> roots_;
+};
 
 } // end namespace gc
