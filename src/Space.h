e57cb828c099de509ffd8d8d14d8fde985f59dae
Jesse A. Tov <jesse.tov@gmail.com>
2016-05-19 19:48:25 -0500

    Added comments.

--- src/Space.h
+++ src/Space.h
@@ -1,5 +1,9 @@
-// Collector_base is the interface between the Collector<T>s and the
-// CollectorManager.
+// Space is the interface between the TypedSpace<T>s and the
+// Collector. Each TypedSpace<T> manages only objects of type T,
+// but to collect properly we need to mark the whole pointer graph
+// (unless we can prove disjointness!). Thus, we provide this
+// interface to the Collector, and it then makes the spaces cooperate.
+
 #pragma once
 
 #include <cstddef>
@@ -14,16 +18,41 @@ namespace internal
 
 class Space
 {
+    // The client of this interface is the `Collector`.
+    friend class ::gc::Collector;
+
+
+    // Our garbage collection proceeds in four phases, which must be
+    // run for each space in turn; that is, every space must run phase 1,
+    // then every space must run phase 2, etc. Here are the phases:
+
+    // Phase 1: Copies every `traced<T>`’s `ref_count_` to `root_count_`.
     virtual void save_counts_()    =0;
+
+    // Phase 2: Decrements `root_count_` for every in-edge coming from
+    // another traced object. When finished, roots will have non-zero
+    // `root_count_`.
     virtual void find_roots_()     =0;
+
+    // Phase 3: Marks the live heap starting with the roots found in the
+    // previous phase.
     virtual void mark_()           =0;
+
+    // Phase 4: Sweeps away the dead heap, deallocating and resetting marks.
     virtual void sweep_()          =0;
 
-    virtual size_t element_size_() =0;
-    virtual size_t capacity_()     =0;
-    virtual size_t used_()         =0;
 
-    friend class ::gc::Collector;
+    // Stats, currently unused.
+
+    // The size of `T` for each `Space<T>`.
+    virtual size_t element_size_() const =0;
+
+    // The number of objects this `Space` can hold without growing.
+    virtual size_t capacity_() const     =0;
+
+    // Over-approximation of the number of live objects this `Space` currently
+    // holds.
+    virtual size_t used_() const         =0;
 
 protected:
     virtual ~Space() { }
--- src/TypedSpace.h
+++ src/TypedSpace.h
@@ -1,4 +1,5 @@
-// A Collector<T> manages the pointers of one type, T.
+// A `TypedSpace<T>` manages the pointers of one type, `T`. It implements the
+// interface `Space`, which the `Collector` uses to manage it.
 #pragma once
 
 #include "forward.h"
@@ -20,12 +21,15 @@ template <typename T, typename Allocator, typename PAllocator>
 class TypedSpace : private internal::Space
 {
 public:
+    // Returns the singleton instance for allocating objects of type `T`.
     static TypedSpace& instance() noexcept
     {
         static TypedSpace instance_;
         return instance_;
     }
 
+    // Allocates an object of type `T` given parameters to forward to its
+    // constructor.
     template <typename... Args>
     traced_ptr<T, Allocator, PAllocator>
     allocate(Args&&... args)
@@ -37,20 +41,26 @@ public:
     };
 
 private:
+    // The type of pointer we are managing.
     using ptr_t = traced<T>*;
 
+    // Produces a friendly error if the given allocator doesn’t actually
+    // allocate the right type.
     static_assert(std::is_same<traced<T>, typename Allocator::value_type>::value,
                   "Invalid Allocator");
     static_assert(std::is_same<traced<T>*, typename PAllocator::value_type>::value,
                   "Invalid PAllocator");
 
-    Allocator allocator_;
-    Collector& collector_;
-    size_t heap_size_;
-    size_t live_size_;
-    traced<T>* pages_;
-    traced<T>* free_list_;
+    Allocator allocator_;   // For allocating pages of `traced<T>`s
+    Collector& collector_;  // The collector managing this space
+    size_t heap_size_;      // The capacity of this space, in objects
+    size_t live_size_;      // The number of used slots
+    traced<T>* pages_;      // Linked list of pages to allocate in — unused!
+    traced<T>* free_list_;  // Linked list of free object slots
 
+    // Constructs a `TypedSpace`, which includes registering it with a
+    // collector. By default it uses the global collector. (There is
+    // currently nothing useful we can do with non-global spaces/collectors.)
     TypedSpace(Collector& collector = Collector::instance())
             : collector_{collector}
             , heap_size_{0}
@@ -61,6 +71,9 @@ private:
         collector_.register_space_(*this);
     }
 
+    // Adds a page that can hold `size` objects: Requests memory for the
+    // objects from the allocator, adds its slots to the free list, and
+    // adds the new page to the front of the page list.
     void add_page_(size_t size)
     {
         ptr_t page = allocator_.allocate(size);
@@ -73,6 +86,7 @@ private:
         heap_size_ += size - 1;
     }
 
+    // Adds the given pointer to the free list.
     void add_to_free_list_(ptr_t ptr)
     {
         ptr->initialize_free_(free_list_);
@@ -127,7 +141,7 @@ private:
     }
 
     template <typename F>
-    void for_heap(F f)
+    void for_heap_(F f)
     {
         for (ptr_t page = pages_; page != nullptr; page = page->next_page_())
             for (size_t i = 1; i < page->page_size_(); ++i)
@@ -135,16 +149,26 @@ private:
                     f(&page[i]);
     }
 
+    // The remaining member functions are implementations of Space’s pure
+    // virtual members.
+
+    //
+    // Collection interface — the four phases of collection (see Space.h)
+    //
+
+    // GC phase 1: Copies every ref_count_ to root_count_
     virtual void save_counts_() override
     {
-        for_heap([](ptr_t ptr) {
+        for_heap_([](ptr_t ptr) {
             ptr->root_count_() = ptr->ref_count_();
         });
     }
 
+    // GC phase 2: Decrements root_count_ for every in-edge coming from
+    // another traced object.
     virtual void find_roots_() override
     {
-        for_heap([](ptr_t ptr) {
+        for_heap_([](ptr_t ptr) {
             ::gc::internal::trace_(ptr->object_(), [](auto sub_ptr) {
                 if (sub_ptr != nullptr)
                     --sub_ptr->root_count_();
@@ -152,17 +176,19 @@ private:
         });
     }
 
+    // GC phase 3: Marks the live heap via tracing DFS.
     virtual void mark_() override
     {
-        for_heap([](ptr_t ptr) {
+        for_heap_([](ptr_t ptr) {
             if (ptr->root_count_() > 0)
                 mark_recursively_(ptr);
         });
     }
 
+    // GC phase 4: Sweeps away the dead heap, deallocating and resetting marks.
     virtual void sweep_() override
     {
-        for_heap([this](ptr_t ptr) {
+        for_heap_([this](ptr_t ptr) {
             if (ptr->mark_)
                 ptr->mark_ = false;
             else
@@ -170,22 +196,28 @@ private:
         });
     }
 
-    virtual size_t element_size_() override
+    //
+    // Stats interface – see comments in `Space`
+    //
+
+    virtual size_t element_size_() const override
     {
         return sizeof(T);
     }
 
-    virtual size_t capacity_() override
+    virtual size_t capacity_() const override
     {
         return heap_size_;
     }
 
-    virtual size_t used_() override
+    virtual size_t used_() const override
     {
         return live_size_;
     }
 };
 
+// Allocates an object of type `T` given parameters to forward to its
+// constructor.
 template <typename T,
           typename Allocator  = std::allocator<traced<T>>,
           typename PAllocator = std::allocator<traced<T>*>,
